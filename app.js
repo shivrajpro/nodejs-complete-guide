7100328be0a1e4eaa3531be32cf6b79a3070a0fa
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-18 20:04:25 +0530

    outsourcing routes to different files

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="post"> <input type="text" placeholder="enter title" name="title"> <button type="submit">Add Product</button> </form>');
-} )
-
-app.use('/product', (req, res, next)=>{
-    console.log('body=',req.body);
-    res.redirect('/');
-} )
-
-app.use('/',(req, res, next)=>{
-    console.log("In another middleware");
-    res.send("<h1>Hello from Express!</h1>")
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000); //creates a server on port
