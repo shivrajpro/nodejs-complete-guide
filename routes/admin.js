45e7ea02cd01146627565f4c24c63a21da21bd1d
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-18 20:37:51 +0530

    adding html files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
+// admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="post"> <input type="text" placeholder="enter title" name="title"> <button type="submit">Add Product</button> </form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    // res.send('<form action="/admin/add-product" method="post"> <input type="text" placeholder="enter title" name="title"> <button type="submit">Add Product</button> </form>');
 } )
 
-router.post('/product', (req, res, next)=>{
+// admin/add-product => POST
+router.post('/add-product', (req, res, next)=>{
     console.log('body=',req.body);
     res.redirect('/');
 } )
