577224bf9c6e9ed75076aa7dce2818561e0485d7
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-18 18:06:59 +0530

    assignment 2 solution

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes.reqHandler);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use('/',(req, res, next)=>{
+    console.log("in a first middleware");
+    next();
+})
+
+app.use('/users',(req, res, next)=>{
+    res.send("<h1>Show list of users!</h1>")
+})
+
+app.use('/',(req, res, next)=>{
+    console.log("In another middleware");
+    res.send("<h1>Hello from Express!</h1>")
+})
+
+app.listen(3000); //creates a server on port
