be8ce1d14b46d985a7f5d01c9ae029b6a73b9bc6
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-21 00:35:27 +0530

    refactoring product.js in models

--- models/product.js
+++ models/product.js
@@ -1,43 +1,34 @@
 // const products = [];
-const path = require('path');
-const fs = require('fs');
-module.exports = class Product{
-    constructor(t){
-        this.title = t;
-    }
+const path = require("path");
+const fs = require("fs");
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
-    save(){
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'products.json'
-        );
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) return cb([]);
+    return cb(JSON.parse(fileContent));
+  });
+};
 
-        fs.readFile(p, (err, fileContent)=>{
-            let products = [];
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-            if(!err && fileContent){
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-            fs.writeFile(p, JSON.stringify(products), err=>{
-                console.log(err);
-            })
-        })
-    }
-
-    static fetchAll(cb){
-        // return products;
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'products.json'
-        );
-
-        fs.readFile(p, (err, fileContent)=>{
-            if(err) return cb([]);
-            return cb(JSON.parse(fileContent));
-        })
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
