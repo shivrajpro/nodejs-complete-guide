b9b53f8ae4a77ca910d9c242171282281e9746f1
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-25 22:47:03 +0530

    adding User schema - mongoose

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); //public folder contains static resources like js and css
 
-// app.use((req, res, next) => {
-//   User.findById('6380559458986011e8d9a617')
-//   .then(user=>{
-//     // console.log("USER",user);
-//     req.user = new User(user.username, user.email, user._id, user.cart);
-//     next();
-//   })
-//   .catch(e=>{
-//     console.log(e);
-//   })
-// });
+app.use((req, res, next) => {
+  User.findById('6380559458986011e8d9a617')
+  .then(user=>{
+    // console.log("USER",user);
+    req.user = user;
+    next();
+  })
+  .catch(e=>{
+    console.log(e);
+  })
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views"); //folder in which our templates are kept
@@ -37,6 +37,23 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://shivraj:shiv@cluster0.bu9ow60.mongodb.net/shop')
 .then(result=>{
   console.log("CONNECTED");
+
+  User.findById('6380f66dfdf40701a99c47c5')
+  .then(user=>{
+
+    if(!user){
+      const user = new User({
+        username:"shivraj",
+        email:"shivraj@test.com",
+        cart:{
+          items:[]
+        }
+      });
+    
+      user.save();
+    }
+  })
+  .catch(e=>console.log(e))
   app.listen(3000);
 }).then(e=>{
   console.log(e);
--- models/user.js
+++ models/user.js
@@ -1,149 +1,25 @@
-const mongodb = require("mongodb");
-const getDb = require("../util/database").getDb;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-class User {
-  constructor(username, email, id, cart) {
-    this.username = username;
-    this.email = email;
-    this._id = id ? new mongodb.ObjectId(id) : null; //will be undefined in case of new product
-    this.cart = cart;
-  }
-
-  save() {
-    const db = getDb();
-    let dbOp;
-
-    if (this._id) dbOp = db.collection("users").insertOne(this);
-    else {
-      dbOp = db
-        .collection("users")
-        .updateOne({ _id: this._id }, { $set: this });
-    }
-
-    return dbOp;
-  }
-
-  addToCart(product) {
-    let newQuantity = 1;
-    if (!this.cart.items) this.cart.items = [];
-
-    let existingProductIndex = this.cart.items.findIndex(
-      (i) => i.productId.toString() === product._id.toString()
-    );
-    let updatedCartItems = [...this.cart.items];
-
-    if (existingProductIndex != -1) {
-      const oldQuantity = this.cart.items[existingProductIndex].quantity;
-      newQuantity = oldQuantity + 1;
-      updatedCartItems[existingProductIndex].quantity = newQuantity;
-    } else {
-      updatedCartItems.push({
-        productId: new mongodb.ObjectId(product._id),
-        quantity: newQuantity,
-      });
-    }
-
-    const updatedCart = { items: updatedCartItems };
-    const db = getDb();
-
-    return db
-      .collection("users")
-      .updateOne(
-        { _id: new mongodb.ObjectId(this._id) },
-        { $set: { cart: updatedCart } }
-      );
-  }
-
-  getCart() {
-    // need to return all the products info which are in user's cart
-    // also we should check if a product in cart items is present in products collection
-    // so that products delete by admin will not be present in any cart
-    const db = getDb();
-    const productIds = this.cart.items.map((i) => i.productId);
-
-    return db
-      .collection("products")
-      .find({ _id: { $in: productIds } })
-      .toArray()
-      .then((products) => {
-        return products.map((p) => {
-          const productQty = this.cart.items.find(
-            (i) => i.productId.toString() === p._id.toString()
-          ).quantity;
-
-          return {
-            ...p,
-            quantity: productQty,
-          };
-        });
-      });
-  }
-
-  deleteCartItem(productId, userId) {
-    const db = getDb();
-    const updateCartItems = this.cart.items.filter(
-      (i) => i.productId.toString() !== productId
-    );
-
-    return db.collection("users").updateOne(
-      { _id: new mongodb.ObjectId(this._id) },
+const userSchema = Schema({
+  username: {
+    type:String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [
       {
-        $set: {
-          cart: {
-            items: updateCartItems,
-          },
-        },
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true },
       }
-    );
-  }
+    ]
+  },
+});
 
-  addOrder() {
-    //put the cart items in orders and empty the cart
-    const db = getDb();
 
-    // get cart gives the cart items with product info
-    return this.getCart().then((products) => {
-      const order = {
-        items: products,
-        user: {
-          _id: this._id,
-          username: this.username,
-        },
-      };
-      db.collection("orders").insertOne(order);
-
-      return db
-        .collection("users")
-        .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
-    });
-  }
-
-  getOrders() {
-    const db = getDb();
-
-    return db
-      .collection("orders")
-      .find({ "user._id": new mongodb.ObjectId(this._id) })
-      .toArray();
-  }
-
-  static findById(userId) {
-    const db = getDb();
-
-    return db
-      .collection("users")
-      .find({ _id: new mongodb.ObjectId(userId) })
-      .next();
-    // .findOne({_id:new mongodb.ObjectId(userId)})
-  }
-
-  static deleteById(userId) {
-    const db = getDb();
-
-    return db
-      .collection("users")
-      .deleteOne({ _id: new mongodb.ObjectId(userId) });
-  }
-}
-
+const User = mongoose.model("User", userSchema);
 module.exports = User;
