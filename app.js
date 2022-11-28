fcb7cbb763c33cf23494d47e369743470045d695
shivrajpro <shivrajhulbatte@gmail.com>
2022-11-28 14:50:08 +0530

    installing connect-mongodb-session

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const path = require("path");
 
-const mongoose = require('mongoose');
+const MONGODB_URI =
+  "mongodb+srv://shivraj:shiv@cluster0.bu9ow60.mongodb.net/shop";
+const mongoose = require("mongoose");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const app = express();
-
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions", //name of the collection where session will be stored
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); //public folder contains static resources like js and css
 
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
 app.use((req, res, next) => {
-  User.findById('6380f66dfdf40701a99c47c5')
-  .then(user=>{
-    // console.log("USER",user);
-    req.user = user;
-    next();
-  })
-  .catch(e=>{
-    console.log(e);
-  })
+  User.findById("6380f66dfdf40701a99c47c5")
+    .then((user) => {
+      // console.log("USER",user);
+      req.user = user;
+      next();
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
 
 app.set("view engine", "ejs");
@@ -36,27 +51,28 @@ app.use(shopRoutes);
 app.use(authRoutes);
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://shivraj:shiv@cluster0.bu9ow60.mongodb.net/shop')
-.then(result=>{
-  console.log("CONNECTED");
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("CONNECTED");
 
-  User.findById('6380f66dfdf40701a99c47c5')
-  .then(user=>{
+    User.findById("6380f66dfdf40701a99c47c5")
+      .then((user) => {
+        if (!user) {
+          const user = new User({
+            username: "shivraj",
+            email: "shivraj@test.com",
+            cart: {
+              items: [],
+            },
+          });
 
-    if(!user){
-      const user = new User({
-        username:"shivraj",
-        email:"shivraj@test.com",
-        cart:{
-          items:[]
+          user.save();
         }
-      });
-    
-      user.save();
-    }
+      })
+      .catch((e) => console.log(e));
+    app.listen(3000);
   })
-  .catch(e=>console.log(e))
-  app.listen(3000);
-}).then(e=>{
-  console.log(e);
-})
+  .then((e) => {
+    console.log(e);
+  });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 exports.getLogin = (req, res, next)=>{
+    console.log("SESSION",req.session);
     // console.log("Cookie",req.get("Cookie").split("=")[1]);
     const isLoggedIn = req.get("Cookie")?.split("=")[1];
     res.render("auth/login", {
@@ -9,6 +10,7 @@ exports.getLogin = (req, res, next)=>{
 }
 
 exports.postLogin = (req, res, next)=>{
-    res.setHeader("Set-Cookie","loggedIn=true; Max-Age=10");
+    // res.setHeader("Set-Cookie","loggedIn=true; Max-Age=10");
+    req.session.isLoggedIn = true;
     res.redirect('/')
 }
